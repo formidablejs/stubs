fefa4d83538e699f9e330727231231d8f5ed3ef8
Donald Pakkies <donaldpakkies@gmail.com>
2022-03-04 18:31:55 +0000

    feat: added a command stub

---
+++ src/stubs/command/command.js
@@ -0,0 +1,31 @@
+const Stub = require('../../Stub');
+
+module.exports = class Command extends Stub {
+    /**
+	 * @inheritdoc
+	 */
+	get data() {
+		return {
+			'class': this.realClassName,
+			'signature': this.signature
+		};
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	get destination() {
+		return 'app/Console/Commands';
+	}
+
+    /**
+	 * Signature used by the command.
+	 */
+	get signature() {
+		return pluralize(this.realClassName)
+			.replace(/([A-Z])/g, ':$1')
+			.trim()
+			.replace(/^\_+/, '')
+			.toLowerCase();
+	}
+}
