0d7fe74d14609c7cf645f4bff56d3cbd46fbfe55
Donald Pakkies <donaldpakkies@gmail.com>
2022-11-16 18:26:48 +0200

    feat: make definition

--- src/stubs/migration/migration.js
+++ src/stubs/migration/migration.js
@@ -49,6 +49,10 @@ module.exports = class Migration extends Stub {
 				type: Boolean,
 				default: false,
 			},
+			schema: {
+				type: String,
+				required: false
+			}
 		};
 	}
 
@@ -65,7 +69,57 @@ module.exports = class Migration extends Stub {
 	get data() {
 		return {
 			'table': this.options.table,
+			'schema': this.getSchema
 		};
+	}
+
+	/**
+	 * Create new schema.
+	 *
+	 * @returns {string}
+	 */
+	get getSchema() {
+		if (!this.options.schema) return '';
+
+		const definitions = this.options.schema.split(',');
+
+		const lines = [
+			"\t\ttable.bigIncrements().primary()",
+		];
+
+		for (const key in definitions) {
+			if (Object.hasOwnProperty.call(definitions, key)) {
+				const element = definitions[key];
+				let [ name, type ] = element.split(':')
+
+				type = type.split('.')[0]
+
+				let line = `table.${type}("` + name.trim() + '")'
+
+				let chain = element.split(':')[1].split('.')
+
+				chain = chain.slice(1)
+
+				for (const key in chain) {
+					if (Object.hasOwnProperty.call(chain, key)) {
+						const method = chain[key];
+						line += `.${method}()`
+					}
+				}
+
+				if (!lines.includes(`\t\t${line}`)) {
+					lines.push(`\t\t${line}`)
+				}
+			}
+		}
+
+		const timestamps = "\t\ttable.timestamps(true, true)"
+
+		if (!lines.includes(timestamps)) {
+			lines.push(timestamps);
+		}
+
+		return lines.join(';\n') + ';'
 	}
 
 	/**
